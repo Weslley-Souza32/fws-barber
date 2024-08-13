27eea901ebc962e90df4cb8fc440361cd1af9b99
WeslleyDev <weslleyjunio3032@gmail.com>
2024-08-13 10:48:12 -0300

    feat: adds booking items

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -10,15 +13,17 @@ const Home = () => {
       {/* Header */}
       <Header />
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Olá, Weslley!</h2>
         <p>Terça-Feira, 13 de Agosto.</p>
-
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
+        {/* Banner */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/banner-01.png"
@@ -27,6 +32,28 @@ const Home = () => {
             alt="Agende nos melhores com FSW Barber"
           />
         </div>
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Div da esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FWS</p>
+              </div>
+            </div>
+            {/* Div da Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">13</p>
+              <p className="text-sm">11:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
